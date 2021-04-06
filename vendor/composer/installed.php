46957e28e50eb58790d91f49d3e8bd782ae1bd49
Wasamii <erwann.wansch@hotmail.fr>
2021-04-06 12:14:29 +0200

    Add composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'b60e5dfa8b9cb7ef39ef63fe980e8a02a60e79df',
+    'name' => 'test/ons',
+  ),
+  'versions' => 
+  array (
+    'test/ons' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b60e5dfa8b9cb7ef39ef63fe980e8a02a60e79df',
+    ),
+  ),
+);
