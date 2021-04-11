c84e7cd7a3ed74b4b3a4a91cf0185c71c154609c
Wasamii <erwann.wansch@hotmail.fr>
2021-04-11 14:28:43 +0200

    Add action login etc...

--- index.php
+++ index.php
@@ -1,23 +1,49 @@
 <?php
+
 session_start();
 require('controller/controller.php');
-try{
-    if(isset($_GET['action'])){
-        $callAction =$_GET['action'];
-
-    }else{
-        $callAction = "";
-    }
-    switch($callAction){
-        case'basicglyphers':
-            basicglypher();
-        break;
-
-
-
-        default: basicglypher();
+//chercher à faire les choses en POO
+class Index extends Controller{
+    public function __construct() {
+        // die(var_dump('dans le constructeur'));
+        $instancecontroller = new Controller();
+        try{
+            if(isset($_GET['action'])){
+                $callAction =$_GET['action'];
+        
+            }else{
+                $callAction = "";
+            }
+            switch($callAction){
+                //affichage de base
+                case'basicglyphers':
+                    $instancecontroller->basicglypher();
+                break;
+                //Redirige vers inscription
+                case 'members':
+                    $instancecontroller->signup();
+                break;
+                //Connexion
+                case 'validSignup':
+                    $pseudo = $_POST['pseudo'];
+                    $instancecontroller->verifPseudo($pseudo);
+                    $instancecontroller->postsignup();
+                break;
+                case'getlog':
+                    $instancecontroller->verifyLogin();
+                break;
+                case'disconnect':
+                    $instancecontroller->disconnect();
+                break;
+                //Affiche la liste de base non membre connecté
+                default: 
+                    $instancecontroller->basicglypher();
+                break;
+            }
+        }
+        catch (Exception $e){
+            echo "Error:" . $e->getMessage();
+        }
     }
 }
-catch (Exception $e){
-    echo "Error:" . $e->getMessage();
-}
+$instanceIndex = new Index();
