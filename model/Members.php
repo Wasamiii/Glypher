3db15be7f775c8eeb84b077155eb83322ebc93be
Wasamii <erwann.wansch@hotmail.fr>
2021-04-11 14:24:35 +0200

    Add Login/signup/disconnect

--- model/Members.php
+++ model/Members.php
@@ -3,7 +3,31 @@ namespace Wamp\www\model;
 require_once('model/Manager.php');
 class Members extends Manager
 {
-
+    public function postsignup($pseudo,$email,$pass_hache){
+        $db = $this->dbConnect();
+        $register = $db->prepare('INSERT INTO users(pseudo, email, password, date) VALUES(:pseudo, :email, :password,  NOW())');
+        $register->execute(array(
+            'pseudo' => $pseudo,
+            'email' => $email,
+            'password' => $pass_hache));
+        echo "Le compte à été créée";
+        return $register;      
+    }
+    public function getsignup($pseudo){
+        $db = $this->dbConnect();
+        $verifpseudo = $db->prepare('SELECT pseudo FROM users WHERE pseudo = ?');
+        $verifpseudo -> execute(array($pseudo));
+        $count = $verifpseudo -> rowCount();
+        return $count;
+    }
+    public function getlogin(){
+        $db = $this->dbConnect();
+        $pseudo = $_POST['pseudo'];
+        $req = $db->prepare('SELECT * FROM users WHERE pseudo = :pseudo');
+        $req->execute(['pseudo' => $pseudo]);
+        $resultat = $req->fetch();
+        return $resultat;
+    }
 }
 
 ?>
