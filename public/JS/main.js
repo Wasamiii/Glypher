89295c00ba7469e69443d236d1f0cb2decd60f6c
Wasamii <erwann.wansch@hotmail.fr>
2021-04-15 11:16:08 +0200

    add link to worldstate WFCD

---
+++ public/JS/main.js
@@ -0,0 +1,7 @@
+const worldstateData = await (require('request-promise'))('http://content.warframe.com/dynamic/worldState.php');
+
+const WorldState = require('warframe-worldstate-parser');
+
+const ws = new WorldState(worldstateData);
+
+console.log(ws.alerts[0].toString());
