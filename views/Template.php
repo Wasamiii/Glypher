90f0e473b0ca3cc2bdcc7de7b796b87778da6169
Wasamii <erwann.wansch@hotmail.fr>
2021-04-15 14:41:44 +0200

    Add font awesome and link main.js
    For future logos and timers

--- views/Template.php
+++ views/Template.php
@@ -6,13 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?></title>
     <link rel="stylesheet" href="public/CSS/style.css">
-    <script src="JS/main.js" type="text/javascript"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
 </head>
-    <header>
+<header>
     <?php require_once("menu.php");?>
-    </header>
+</header>
 <body>
     <?=$content?>
 </body>
     <footer></footer>
+        <script src="JS/main.js" type="text/javascript"></script>
 </html>
