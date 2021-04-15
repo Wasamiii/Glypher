880431bd1d4f10b2ecee324c97d0223d45da5e3c
Wasamii <erwann.wansch@hotmail.fr>
2021-04-15 11:15:49 +0200

    add link to main.js

--- views/Template.php
+++ views/Template.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?></title>
     <link rel="stylesheet" href="public/CSS/style.css">
+    <script src="JS/main.js" type="text/javascript"></script>
 </head>
     <header>
     <?php require_once("menu.php");?>
--- views/basicsglypher.php
+++ views/basicsglypher.php
@@ -1,6 +1,6 @@
 <?php $title = 'Glyphers'?>
 <?php ob_start(); ?>
-
+<script src="JS/main.js" type="text/javascript"></script>
 <!--Ici se trouvera la totalité des glyphes mais non cochables car utilisateurs non inscrit -->
 
 <?php $content = ob_get_clean(); ?>
