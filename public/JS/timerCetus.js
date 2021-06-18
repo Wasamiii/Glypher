c4614f774097d8094ba27b5eb62f870b625a5797
Wasamii <erwann.wansch@hotmail.fr>
2021-06-18 11:17:43 +0200

    Hotfix for timer cetus

--- public/JS/timerCetus.js
+++ public/JS/timerCetus.js
@@ -1,44 +1,51 @@
 class TimerCetus{
     constructor(){    
+    this.interval = 0;
+    this.countCetus = document.getElementById('countdown');
+    this.attributeCetus = document.getElementById("timer");
     }
     initTimerCetus(){
-        let attributeCetus = document.getElementById("#timer");
         let CetusIsDay = document.getElementById('#day');
         let CetusIsNight = document.getElementById('#night');
     }
     callApiToCetus(){
-        let countCetus = document.getElementById('countdown');
+        
         let funcCountCetus =()=>{
-            let callApi = new Api();
-            callApi.syndicate();
             let parseCetus;
             //Problème les 2 values en parse ne bougent pas donc soit -date soit elle doivent bouger d'elles même
-            let cetus_JSON = localStorage.getItem('CetusTimers');
-            if (localStorage.getItem('CetusTimers')== null){
+            let cetus_JSON = sessionStorage.getItem('CetusTimers');
+            if (sessionStorage.getItem('CetusTimers')== null){
                 let callerApi = new Api();
                 callerApi.syndicate();
-                
             }else{
                 parseCetus = JSON.parse(cetus_JSON);
                 // console.log(parseCetus);
                 const get_timestamp_eidos_expiry = Math.floor(parseCetus["Expiry"]["$date"]["$numberLong"] / 1000);
-                console.log(get_timestamp_eidos_expiry);
                 //fonctionne pas mais à voir comment le faire fonctionner
                 const get_timestamp_eidos_activation = Math.floor(parseCetus["Activation"]["$date"]["$numberLong"] / 1000);
-                console.log(get_timestamp_eidos_activation);
+                // console.log(get_timestamp_eidos_activation);
     
                 const date = Date.now();
                 let time_date = Math.floor(date / 1000);
-                //en cours de test (non concluant) retourne un timer incohérent
-                const timerEidos = get_timestamp_eidos_expiry - time_date;
-                console.log(timerEidos);
-                let eidos_H = parseInt(((timerEidos /60)/60)%60);
-                let eidos_M = parseInt((timerEidos /60)%60);
-                let eidos_S = parseInt(timerEidos %60);
-                countCetus.innerHTML = eidos_H +"h"+ eidos_M +"m"+ eidos_S +"s";
+                //
+                let timerEidos = get_timestamp_eidos_expiry - time_date;
+                if(timerEidos>3000){
+                    //day
+                    this.attributeCetus.setAttribute('data-cycle', 'day');
+                    timerEidos = timerEidos - 3000;
+                }else{
+                    //night
+                    this.attributeCetus.setAttribute('data-cycle', 'night');
+                }
+                let eidos_S = Math.floor(timerEidos %60);
+                let eidos_M = (((timerEidos - eidos_S) /60)%60);
+                let eidos_H = Math.floor(((timerEidos - eidos_S) /60)/60);
+                this.countCetus.innerHTML = eidos_H +"h"+ eidos_M +"m"+ eidos_S +"s";
             }
         };
-        funcCountCetus();
+        //setInterval fonctionne mais cause de l'appel de la fonction fait des appel d'api à chaque fois ce qui est pas bon 
+        //cause le if else qui englobe trop les choses
+        this.interval = setInterval(funcCountCetus,1000);
     }
 }
 let timercetus = new TimerCetus();
