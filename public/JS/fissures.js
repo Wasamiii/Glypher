98777f38b7c1cc812f500f40a2c2cc14f3a714b6
Wasamii <erwann.wansch@hotmail.fr>
2021-06-28 17:31:21 +0200

    add test for timer fissures

--- public/JS/fissures.js
+++ public/JS/fissures.js
@@ -1,5 +1,22 @@
 class Fissures{
     constructor(){
-        
+    this.interval = 0;
+    this.countCetus = document.getElementById('countdown');
+    this.attributeCetus = document.getElementById("timer");
+    }
+    callAPIfissures(){
+        let functionToFissures = ()=>{
+            let fissureParse;
+            let fissuressession = sessionStorage.getItem('Fissures');
+            if(sessionStorage.getItem('Fissures') == null){
+                refreshAPIfissures();
+            }else{
+                fissureParse = JSON.parse(fissuressession);
+                console.log(fissureParse);
+            }
+        };
+        functionToFissures();
     }
 }
+let timerFissures = new Fissures();
+timerFissures.callAPIfissures();
