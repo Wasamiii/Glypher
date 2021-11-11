205e716bc00a0416f5820876fedc61354712387e
Wasamii <erwann.wansch@hotmail.fr>
2021-11-11 14:52:46 +0100

    Call BDD for glyph

--- model/Glyph.php
+++ model/Glyph.php
@@ -1,18 +1,19 @@
 <?php
 namespace model;
 use model\Manager;
-
+require_once('Manager.php');
 class Glyph extends Manager{
     public function __construct() {
 
     }
     public function GetGlyph(){
         $db = $this->dbConnect();
-        $getglyph = $db -> prepare('SELECT * 
+        $reqgetglyph = $db -> query('SELECT * 
         FROM glyphs 
         ORDER BY  title ASC');
-        $getglyph -> execute();
-        return $getglyph;
+        //$reqgetglyph -> execute(array());
+        //$getglyph = $reqgetglyph->fetchAll();
+        return $reqgetglyph;
     }
 }
 ?>
