56982971bceba5275cc0e29988051c2458f1015d
Wasamii <erwann.wansch@hotmail.fr>
2021-11-11 15:00:21 +0100

    change require_once to require and add style.js

--- views/basicsglypher.php
+++ views/basicsglypher.php
@@ -1,7 +1,8 @@
 <?php $title = 'Glyphers'?>
 <?php ob_start(); ?>
     <?php require_once('views/timers.php');?>
-    <?php require_once('views/glyph.php'); ?>
+    <?php require('views/glyph.php'); ?>
+    
 <!--Ici se trouvera la totalité des glyphes mais non cochables car utilisateurs non inscrit -->
 <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script> 
 <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
@@ -9,5 +10,6 @@
 <script src="public/JS/timerCetus.js"></script>
 <script src="public/JS/fissures.js"></script>
 <script src="public/JS/barokiteer.js"></script>
+<script src="public/JS/style.js"></script>
 <?php $content = ob_get_clean(); ?>
 <?php require_once('views/template.php');?>
