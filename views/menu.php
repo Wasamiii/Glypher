b5e5fac19a62f10502a6dffdcd2d47e1b531ec45
Wasamii <erwann.wansch@hotmail.fr>
2021-11-18 15:58:44 +0100

    Change french to english

--- views/menu.php
+++ views/menu.php
@@ -10,17 +10,17 @@
 if (isset($_SESSION['id']) AND isset($_SESSION['pseudo']))
     {
             ?>       
-            <a href="index.php?action=disconnect" id="disconnect">Deconnection</a>
+            <a href="index.php?action=disconnect" id="disconnect">Logout</a>
            
 <?php        
         if($_SESSION['admin'] == "1"){?>
-        <a href="index.php?action=admin" id="administration">administration</a>
+        <a href="index.php?action=admin" id="administration">admin</a>
 <?php
         }
 
 ?>
 </div>
- <p id="bonjourPseudo"><?php echo 'Bonjour ' . $_SESSION['pseudo'];?></p>
+ <p id="bonjourPseudo"><?php echo 'Hello ' . $_SESSION['pseudo'];?></p>
 
  <?php
     }else{   
@@ -28,9 +28,9 @@ if (isset($_SESSION['id']) AND isset($_SESSION['pseudo']))
 <div id="allForm">
 <form action="index.php?action=getlog" method="post" id="connexionForm">
         <input type="name" name="pseudo" placeholder="Pseudo" class="inputSigin" required>
-        <input type="password" name="password" placeholder="Mot de passe" class="inputSigin" required>
-        <input type="submit" value="connexion" name="signin" id="connexionSigin">
-        <a href="index.php?action=members" id="singupLink">Inscription</a>
+        <input type="password" name="password" placeholder="Password" class="inputSigin" required>
+        <input type="submit" value="Login" name="signin" id="connexionSigin">
+        <a href="index.php?action=members" id="singupLink">Singup</a>
 </form>
 </div>
 </div>
