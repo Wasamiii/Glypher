1fa7d471d593bba4a7037bc48c594aa714d95024
Wasamii <erwann.wansch@hotmail.fr>
2022-01-18 14:38:02 +0100

    add admin page

--- views/admin-submit.php
+++ views/admin-submit.php
@@ -1,15 +1,14 @@
 <div id="contain-glyph-admin">
-<?php if ($_SESSION['admin'] === "1"){
-?>
+    <ul class="listg-admin">
     <?php while($data = $getglyph->fetch()){?>
-    <figure id="figure-admin">
-        <img src="public/IMG/submit/<?= $data['img_submit'] ?>" id="img-glyph">
-        <figcaption>
+        <li class="list-admin">
             <p id="title-of-glyph"><?= $data['title_submit'] ?></p>
-        </figcaption>
-    </figure>
+            <img src="public/IMG/submit/<?= $data['img_submit'] ?>" id="img-glyph">
+            <div class="contain-btn-admin">
+                <a class="btn-admin" id="btn-valid-admin">✔</a>
+                <a class="btn-admin" id="btn-suppr-admin">❌</a>
+            </div>
+        </li>
     <?php } ?>
-<?php
-}
-?>
+    </ul>
 </div>
--- views/admin.php
+++ views/admin.php
@@ -1,6 +1,11 @@
-<?php $title='Admin'; ?>
+<?php $title='Admin' ?>
+<?php if ($_SESSION['admin'] === "1"){
+?>
 <?php ob_start();?>
-
-<?php $content = ob_get_contents();?>
-<?php require("views/Template.php")?>
 <?php require("views/admin-submit.php") ?>
+
+<?php $content = ob_get_clean();?>
+<?php require_once("views/Template.php")?>
+<?php
+}
+?>
