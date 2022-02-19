846b8a98020a361182ea6d90dc0bbefa77a8a85a
Wasamii <erwann.wansch@hotmail.fr>
2022-02-19 12:07:37 +0100

    big commit for valid/suppr/modify post admin

--- controller/Controller.php
+++ controller/Controller.php
@@ -13,21 +13,14 @@ use model\Fissures;
 use model\Members;
 use model\Glyph;
 
-class Controller{
-    public function __construct(){
-
+class Controller
+{
+    public function __construct()
+    {
     }
-    // function testGlyph(){
-    //     $glypher = new Glyph();
-    //     $getglyph = $glypher->GetGlyph();
-    //     return $getglyph;
-
-    // }
-    
-
-    //affichage global
-    function basicglypher(){
-        
+    //affichage global + timers
+    public function basicglypher()
+    {
         $postManager = new Members();
         $Fissures = new Fissures();
         
@@ -41,7 +34,7 @@ class Controller{
         $result = curl_exec($ch);
         if (curl_errno($ch)) {
             echo 'Error:' . curl_error($ch);
-        }else{
+        } else {
             // Afficher le résultat du serveur
             $addBiginningResult = '{';
             $addEndingResult = '}';
@@ -65,7 +58,7 @@ class Controller{
             
                 //il faut touver les différents Node
                 $infos = $Fissures->infosFissures($newResult['ActiveMissions'][$i]['Node'])->fetch();
-                 $timeFissures = $newResult['ActiveMissions'][$i]["Expiry"]["\$date"]["\$numberLong"];
+                $timeFissures = $newResult['ActiveMissions'][$i]["Expiry"]["\$date"]["\$numberLong"];
                 // $countr_Fissurres = floor(($timeFissures/10000) - (time()/1000));
                 //Récupérer les Nodes aussi dans la BDD pour les comparer
                 $node = $Fissures->infosFissures($infos['node']);
@@ -74,44 +67,43 @@ class Controller{
                 if ($node->rowCount()!= null) {
                     $void = substr($newResult['ActiveMissions'][$i]['Modifier'], 5, 1);
                     switch ($void) {
-                    case '1': array_push($arrayT1, array_merge($node->fetch(),array("Expiry"=> $timeFissures)));
+                    case '1': array_push($arrayT1, array_merge($node->fetch(), array("Expiry"=> $timeFissures)));
                     break;
-                    case '2': array_push($arrayT2, array_merge($node->fetch(),array("Expiry"=> $timeFissures)));
+                    case '2': array_push($arrayT2, array_merge($node->fetch(), array("Expiry"=> $timeFissures)));
                     break;
-                    case '3': array_push($arrayT3, array_merge($node->fetch(),array("Expiry"=> $timeFissures)));
+                    case '3': array_push($arrayT3, array_merge($node->fetch(), array("Expiry"=> $timeFissures)));
                     break;
-                    case '4': array_push($arrayT4, array_merge($node->fetch(),array("Expiry"=> $timeFissures)));
+                    case '4': array_push($arrayT4, array_merge($node->fetch(), array("Expiry"=> $timeFissures)));
                     break;
-                    case '5': array_push($arrayT5, array_merge($node->fetch(),array("Expiry"=> $timeFissures)));
+                    case '5': array_push($arrayT5, array_merge($node->fetch(), array("Expiry"=> $timeFissures)));
                     break;
                 }
                 } else {
-                    array_push($notInDB,array("node"=> $newResult['ActiveMissions'][$i]['Node'],"Expiry"=> $timeFissures));
+                    array_push($notInDB, array("node"=> $newResult['ActiveMissions'][$i]['Node'],"Expiry"=> $timeFissures));
                 }
             }
-
         }
-        curl_close ($ch);
-
-
+        curl_close($ch);
         $glypher = new Glyph();
         $getglyph = $glypher->GetGlyph();
         require('views/basicsglypher.php');
     }
     //redirect to submit
-    public function submit(){
+    public function submit()
+    {
         require('views/submit.php');
     }
-    //fonction for add glyph
-    public function addpost($titlePost,$img_submit,$submit_Youtube,$submit_Twitch,$submit_Discord,$submit_Twitter,$submit_Instagram,$submit_Facebook,$submit_Site_1,$submit_Site_2,$desc_submit,$author){
+    //fonction for add glyph on submit table
+    public function addpost($titlePost, $img_submit, $submit_Youtube, $submit_Twitch, $submit_Discord, $submit_Twitter, $submit_Instagram, $submit_Facebook, $submit_Site_1, $submit_Site_2, $desc_submit, $author)
+    {
         $newGlyph = new Glyph();
-        if(isset($_FILES["img_submit"]) && $_FILES['img_submit']['error'] == 0 ){
+        if (isset($_FILES["img_submit"]) && $_FILES['img_submit']['error'] == 0) {
             $fileInfo = pathinfo($_FILES['img_submit']['name']);
             $extension = $fileInfo['extension'];
             $allowedExtensions = ['png'];
             var_dump($fileInfo);
-            if (in_array($extension, $allowedExtensions)){
-                $temp =  explode(".",$_FILES["img_submit"]["name"]);
+            if (in_array($extension, $allowedExtensions)) {
+                $temp =  explode(".", $_FILES["img_submit"]["name"]);
                 $newfilename = $_POST['titlePost'].'.'.end($temp);
                 $newdir = 'public\IMG\submit\\';
                 $newFileDIR = $newdir . $newfilename;
@@ -126,22 +118,22 @@ class Controller{
                     while (file_exists($newdir .$_POST['titlePost'] . '('. $i . ').'. end($temp))) {
                         $i++;
                     }
-                        $newfilename = $_POST['titlePost'] . '('. $i . ').'. end($temp);
-                        var_dump($newfilename);
-                        $img_submit = move_uploaded_file($mod, $newdir . $newfilename);
-                        $img_submit = $newfilename;
-                        var_dump($img_submit);
-                        var_dump($i);
-                }else{
+                    $newfilename = $_POST['titlePost'] . '('. $i . ').'. end($temp);
+                    var_dump($newfilename);
+                    $img_submit = move_uploaded_file($mod, $newdir . $newfilename);
+                    $img_submit = $newfilename;
+                    var_dump($img_submit);
+                    var_dump($i);
+                } else {
                     var_dump("dans le else");
                     $img_submit = move_uploaded_file($mod, $newdir . $newfilename);
                     $img_submit = $newfilename;
                     var_dump($img_submit);
                 }
-            }else{
+            } else {
                 echo "This is not a PNG image.";
             }
-        }else{
+        } else {
             echo "Return Code: ". $_FILES['img_submit']['error'];
         }
         //add on BDD the submit form
@@ -159,91 +151,202 @@ class Controller{
             $desc_submit,
             $author
         );
-       
-        if($addGlyph === false){
+
+        if ($addGlyph === false) {
             die(var_dump("Its not possible to add glyph"));
-        }else{
+        } else {
             header('Location: index.php');
         }
     }
-    public function signup(){
+    public function signup()
+    {
         $sign = new Members();
         require('views/signupView.php');
     }
     // function form signup
-    public function postsignup(){
-    $register = new Members();
-        if(isset($_POST['inscription'])){
-        $pseudo = $_POST['pseudo'];
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-        $cpassword = $_POST['cpassword'];
-        extract($_POST);
+    public function postsignup()
+    {
+        $register = new Members();
+        if (isset($_POST['inscription'])) {
+            $pseudo = $_POST['pseudo'];
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+            $cpassword = $_POST['cpassword'];
+            extract($_POST);
 
-            if(!empty($password) && !empty($cpassword) && !empty($email) && !empty($pseudo)){
-            //2 champ mdp différent retourne faux aussi 
-                if($password == $cpassword){
+            if (!empty($password) && !empty($cpassword) && !empty($email) && !empty($pseudo)) {
+                //2 input password différent = return false
+                if ($password == $cpassword) {
                     $pass_hache = password_hash($password, PASSWORD_DEFAULT);
-                    $signup = $register->postsignup($pseudo,$email,$pass_hache);
-                }else{
-                echo "Les mots de passe sont différents !";
+                    $signup = $register->postsignup($pseudo, $email, $pass_hache);
+                } else {
+                    echo "Les mots de passe sont différents !";
                 }
             }
         }
-        //à modifier en fonction des différentes condition 2 pseudo identiques on retourne faux 
-        if($signup !== false){
-        echo('Impossibilité de s\'inscrire pour l\'instant');
-        
-        }else{
-        header('Location: index.php');
+        //à modifier en fonction des différentes condition 2 pseudo identiques on retourne faux
+        if ($signup !== false) {
+            echo('Impossibilité de s\'inscrire pour l\'instant');
+        } else {
+            header('Location: index.php');
         }
     }
     // verify for valid pseudo
-    public function verifPseudo($pseudo){
+    public function verifPseudo($pseudo)
+    {
         $verifpseudo = new Members();
-        if(isset($_POST['inscription'])){
+        if (isset($_POST['inscription'])) {
             $pseudo = $_POST['pseudo'];
             $pseudos = $verifpseudo -> getsignup($pseudo);
 
-            if($pseudos == 0){
+            if ($pseudos == 0) {
                 echo "Pseudo disponible";
-            }else{
+            } else {
                 echo('Pseudo non disponible');
             }
         }
     }
     // verif for login form
-    public function verifyLogin(){
+    public function verifyLogin()
+    {
         $verifylogin = new Members();
-        if(isset($_POST['signin'])){
+        if (isset($_POST['signin'])) {
             $pseudo = $_POST['pseudo'];
             $password = $_POST['password'];
             $resultat = $verifylogin -> getlogin();
-        // Comparaison du pass envoyé via le formulaire avec la base
+            // Comparaison du pass envoyé via le formulaire avec la base
             $isPasswordCorrect = password_verify($_POST['password'], $resultat['password']);
-            
-            if (!$resultat){
+            if (!$resultat) {
                 echo 'Mauvais identifiant !';
-            }else{
+            } else {
                 if ($isPasswordCorrect) {
                     $_SESSION['id'] = $resultat['user_id'];
                     $_SESSION['pseudo'] = $pseudo;
                     $_SESSION['admin']= $resultat['admin'];
                     header('Location: index.php');
-            }else{
+                } else {
                     echo 'Mauvais mot de passe !';
                 }
             }
         }
     }
-    function admin(){
+    //view admin page
+    public function admin()
+    {
         $glypher = new Glyph();
         $getglyph = $glypher->getsubmit();
         require_once('views/admin.php');
     }
-    //function disconnect
-    function disconnect(){
-        session_destroy();
-        header('Location: index.php');
+    public function valid_submit($id_submit)
+    {
+        $glypher = new Glyph();
+        $get_submit = $glypher->getSubmitModify($id_submit);
+
+        $newfilename = $get_submit['title_submit'] .'.png';
+        $parenDIR = 'public\IMG\\';
+        $oldDIR = '.\public\IMG\submit\\';
+        $oldFileDIR = $oldDIR . $newfilename;
+        $newdir = 'public\IMG\IMG-partenaire-warframe\\';
+        if (file_exists($oldFileDIR)) {
+            if(file_exists($newdir.$newfilename)){
+                var_dump("dans le if");
+                echo "Already exist !";
+                $i = 1;
+                while (file_exists($newdir .$get_submit['title_submit'] . '('. $i . ').png')) {
+                    $i++;
+                }
+                $newfilename = $get_submit['title_submit'] . '('. $i . ').png';
+                var_dump($i);
+                $newFileDIR = $newdir . $newfilename;
+                var_dump($newfilename);
+                $img_submit = copy($oldFileDIR, $newFileDIR);
+                $modPrentDir = chmod($parenDIR,0777);
+                $modDir = chmod($oldDIR,0777);
+                $mod= chmod($oldFileDIR,0777);
+                var_dump($modDir);
+                var_dump($mod);
+                var_dump($oldFileDIR);
+                var_dump($newFileDIR);
+                //problème de permission même avec chmod
+                $img_submit = unlink(realpath($oldFileDIR));
+            }else{
+                echo("doesn't exist!");
+                var_dump($newfilename);
+                $newFileDIR = $newdir . $newfilename;
+                $img_submit = copy($oldFileDIR, $newFileDIR);
+                var_dump($img_submit);
+                // unlink par la suite
+                $modPrentDir = chmod($parenDIR,0777);
+                $modDir = chmod($oldDIR,0777);
+                $mod= chmod($oldFileDIR,0777);
+                var_dump($modDir);
+                var_dump($mod);
+                var_dump($oldFileDIR);
+                //problème de permission même avec chmod
+                $img_submit = unlink(realpath($oldFileDIR));
+            }
+            var_dump($img_submit);
+            $validation_submit = $glypher->addOnGlyph(
+                $get_submit['title_submit'],
+                //surement à modifier
+                $get_submit['img_submit'],
+                $get_submit['submit_Youtube'],
+                $get_submit['submit_Twitch'],
+                $get_submit['submit_Discord'],
+                $get_submit['submit_Twitter'],
+                $get_submit['submit_Instagram'],
+                $get_submit['submit_Facebook'],
+                $get_submit['submit_Site_1'],
+                $get_submit['submit_Site_2'],
+                $get_submit['desc_submit'],
+                $get_submit['id_user']
+            );
+            $suppr_submit = $glypher->supprSubmitGlyph($id_submit);
+            var_dump($get_submit);
+            //header('Location: index.php?action=admin');
+        }
     }
-}
+        //btn delete glyph submit
+        function supprSubmitGlypher($id_submit)
+        {
+            $glypher = new Glyph();
+            $supprGlypher = $glypher->supprSubmitGlyph($id_submit);
+            header('Location: index.php?action=admin');
+        }
+        //btn modify submit glyph (redirect view pages)
+        function getModSubmit($getModSubmit)
+        {
+            $glypher= new Glyph();
+            $getModGlyph = $glypher->getSubmitModify($getModSubmit);
+            require_once('views/modify-admin.php');
+        }
+        //submit modify form and no change validation
+        function modifySubmit($submit_Youtube, $submit_Twitch, $submit_Discord, $submit_Twitter, $submit_Instagram, $submit_Facebook, $submit_Site_1, $submit_Site_2, $desc_submit, $id_submit)
+        {
+            $glypher = new Glyph();
+            $modGlypher = $glypher->modifyGlyph(
+                $submit_Youtube,
+                $submit_Twitch,
+                $submit_Discord,
+                $submit_Twitter,
+                $submit_Instagram,
+                $submit_Facebook,
+                $submit_Site_1,
+                $submit_Site_2,
+                $desc_submit,
+                $id_submit
+            );
+            require_once('views/modify-admin.php');
+            if ($modGlypher == false) {
+                die(var_dump($modGlypher));
+            } else {
+                header('Location: index.php?action=admin');
+            }
+        }
+        //function disconnect
+        function disconnect()
+        {
+            session_destroy();
+            header('Location: index.php');
+        }
+    }
