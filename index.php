b9d155911185d8a9897870b4d06ab2932b1bb454
Wasamii <erwann.wansch@hotmail.fr>
2022-02-19 12:10:47 +0100

    add modify/valid/suppr  and its twitter not tiwtter

--- index.php
+++ index.php
@@ -45,7 +45,7 @@ class Index extends Controller{
                             $_POST['submit_Youtube'],
                             $_POST['submit_Twitch'],
                             $_POST['submit_Discord'],
-                            $_POST['submit_Tiwtter'],
+                            $_POST['submit_Twitter'],
                             $_POST['submit_Instagram'],
                             $_POST['submit_Facebook'],
                             $_POST['submit_Site_1'],
@@ -63,6 +63,41 @@ class Index extends Controller{
                     }else{
                         header("Location: index.php");
                     }
+                break;
+                case 'validation':
+                    $id_submit = $_GET['id_submit'];
+                    //les parametres ne vont pas undefined variables
+                    $instancecontroller->valid_submit($id_submit);
+                break;
+                case'supprSubmit':
+                    $id_submit = $_GET['id_submit'];
+                    $instancecontroller->supprSubmitGlypher($id_submit);
+                break;
+                //Trouver comment récupérer l'id_submit
+                case 'getModifySubmit':
+                    if(isset($_SESSION['admin']) && $_SESSION['admin'] == "1"){
+                        $getModGlypher = $_GET['id_submit'];
+                      $instancecontroller->getModSubmit($getModGlypher);
+                    }else{
+                        header("Location: index.php");
+                    }
+                break;
+                case 'modifySubmit':
+                    $getModGlyph = $_GET['id_submit'];
+                    die(var_dump( $instancecontroller->modifySubmit(
+                        //définir les différents noms
+                        $_POST['modify_Youtube'],
+                        $_POST['modify_Twitch'],
+                        $_POST['modify_Discord'],
+                        $_POST['modify_Twitter'],
+                        $_POST['modify_Instagram'],
+                        $_POST['modify_Facebook'],
+                        $_POST['modify_Site_1'],
+                        $_POST['modify_Site_2'],
+                        $_POST['desc_modify'],
+                        $getModGlyph)));
+                break;
+        
                 //Affiche la liste de base non membre connecté
                 default:
                 $instancecontroller->basicglypher();
