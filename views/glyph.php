4743bd10feb6f33ec945075d34ec5c76b77ae8b8
Wasamiii <erwann.wansch@hotmail.fr>
2022-03-10 11:33:28 +0100

    add if user connected add checkbox

--- views/glyph.php
+++ views/glyph.php
@@ -12,8 +12,11 @@
     </a>
 <?php while($data = $getglyph->fetch()){?>
     <figure class="contain-glyph modal-trigger id_<?= $data['id'] ?>">
+        <?php if(isset($_SESSION['id'])){ ?>
+            <input type="checkbox" name="checkglyph" id="checkglyph">
+            <?php } ?>
         <img src="public/IMG/IMG-partenaire-warframe/<?= $data['img'] ?>" class="img-glyph">
-        <figcaption>
+        <figcaption class="figcaption-glyph">
             <p class="title-of-glyph"><?= $data['title'] ?></p>
         </figcaption>
     </figure>
