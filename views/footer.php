26f37c52864e3bbf930ee083b3a0fff32243dfb4
Wasamiii <erwann.wansch@hotmail.fr>
2022-06-08 15:53:50 +0200

    add footer

---
+++ views/footer.php
@@ -0,0 +1,13 @@
+<div id="contact">
+    <a href="https://github.com/Wasamiii" target="__blank" id="github-link"><img src="public/IMG/Social-network/PNG/GitHub-Mark-Light-32px.png" alt="github icon" id="github-icon" class="dark"></a>
+    <div id="contact-discord">
+        <img src="public/IMG/Social-network/Discord-Logo-Color.svg" alt="contact me in discord">
+        <p id="discord-profile">Wasamii#1000</p>
+    </div>
+</div>
+<div id="div-copyright">
+    <p id="copyright">
+All content copyright Wasamii © 2022.
+All rights reserved.
+</p>
+</div>
