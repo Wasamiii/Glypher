4f625ec4cdf8f6ecae742ed0922d4b2d0421a400
Wasamiii <erwann.wansch@hotmail.fr>
2022-06-22 13:49:26 +0200

    add !empty for social network

--- views/admin.php
+++ views/admin.php
@@ -22,14 +22,30 @@ if ($_SESSION['admin'] === "1"){
             </div>
             <img src="public/IMG/submit/<?= $data['img_submit'] ?>" class="img-glyph-modal">
             <div class="social-network">
-                <a target="_blank" href="<?= $data['submit_Youtube'] ?>"><img class="icon-social" src="public/IMG/Social-network/Youtube_icon-icons.com_66802.svg" alt="Youtube icon"></a>
+                <?php if(!empty($data['submit_Youtube'])){?>
+                    <a target="_blank" href="<?= $data['submit_Youtube'] ?>"><img class="icon-social" src="public/IMG/Social-network/Youtube_icon-icons.com_66802.svg" alt="Youtube icon"></a>
+                <?php } ?>
+                <?php if(!empty($data['submit_Twitch'])){?>
                 <a target="_blank" href="<?= $data['submit_Twitch'] ?>"><img class="icon-social" src="public/IMG/Social-network/twitch_logo_icon_170383.svg" alt="Twitch icon"></a>
+                <?php } ?>
+                <?php if(!empty($data['submit_Discord'])){?>
                 <a target="_blank" href="<?= $data['submit_Discord'] ?>"><img class="icon-social" src="public/IMG/Social-network/discord_101785.svg" alt="Discord icon"></a>
+                <?php } ?>
+                <?php if(!empty($data['submit_Twitter'])){?>
                 <a target="_blank" href="<?= $data['submit_Twitter'] ?>"><img class="icon-social" src="public/IMG/Social-network/Twitter_icon-icons.com_66803.svg" alt="Twitter icon"></a>
+                <?php } ?>
+                <?php if(!empty($data['submit_Instagram'])){?>
                 <a target="_blank" href="<?= $data['submit_Instagram'] ?>"><img class="icon-social" src="public/IMG/Social-network/4202090instagramlogosocialsocialmedia-115598_115703.svg" alt="Imstagram icon"></a>
+                <?php } ?>
+                <?php if(!empty($data['submit_Facebook'])){?>
                 <a target="_blank" href="<?= $data['submit_Facebook'] ?>"><img class="icon-social" src="public/IMG/Social-network/Facebook_icon-icons.com_66805.svg" alt="Facebook icon"></a>
+                <?php } ?>
+                <?php if(!empty($data['submit_Site_1'])){?>
                 <a target="_blank" href="<?= $data['submit_Site_1'] ?>"><img class="icon-social" src="public/IMG/Social-network/emblemweb_93503.svg" alt="Site-1 icon"></a>
+                <?php } ?>
+                <?php if(!empty($data['submit_Site_2'])){?>
                 <a target="_blank" href="<?= $data['submit_Site_2'] ?>"><img class="icon-social" src="public/IMG/Social-network/emblemweb_93503.svg" alt="Site-2 icon"></a>
+                <?php } ?>
             </div>
             <p class="desc-of-glyph"><?php echo($data['desc_submit']); ?></p>
             <div class="contain-btn-admin">
